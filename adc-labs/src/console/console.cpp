ca1852fff456efdb1164f4ab7f393c0f22fc820d
Jason Melton <jmelton@jasonmelton.com>
2025-10-05 15:58:40 -0400

    Add code to display prompt
    This prompt is displayed whenever the carriage return key is detected.

--- adc-labs/src/console/console.cpp
+++ adc-labs/src/console/console.cpp
@@ -2,17 +2,41 @@
 
 #include <Arduino.h>
 
+#define RTRN_CHAR                           0x0Du
 #define CONSOLE_PORT                        Serial
 #define CONSOLE_BAUDRATE                    115200
+
+
+static void display_prompt(void);
+
 
 void console_cfg(void)
 {
     // Prepare the console port for use
     CONSOLE_PORT.begin((long)CONSOLE_BAUDRATE);
+
+    // Display the basic prompt
+    display_prompt();
 }
 
 void console_action(void)
 {
-    // Display the "hello world" message
-    CONSOLE_PORT.print("Hello World!\r\n"); 
+    // Check to see if a character had been sent to the board
+    if (0 < CONSOLE_PORT.available())
+    {
+        // Grab a byte out of the receive buffer
+        const uint8_t in_byte = (uint8_t)(0xFF & CONSOLE_PORT.read());
+
+        // Check to see if the character is the return character
+        if ((uint8_t)RTRN_CHAR == in_byte)
+        {
+            CONSOLE_PORT.print("\r\n");
+            display_prompt();
+        }
+    }
+}
+
+static void display_prompt(void)
+{
+    CONSOLE_PORT.print("$ ");
 }
--- adc-labs/src/console/console.h
+++ adc-labs/src/console/console.h
@@ -1,9 +1,6 @@
 #if !defined(CONSOLE_H)
 #define CONSOLE_H
 
-#define CONSOLE_MAX_LINE_SZ                 80
-
-
 void console_cfg(void);
 void console_action(void);
 
